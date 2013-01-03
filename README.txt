4a3e79c875c2c94a1ebe61d2a431e36d18b32401
Brian Rollins <brollins@medianewsgroup.com>
2013-01-03 09:37:43 -0700

    Initial load.
    Uploading files to GIT

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is CLog (short for Console.log). My aim is to create a tool that allows for better, more visually useful logging with respect to the Firebug API.
+
+Copyright 2013, Brian Rollins (me@brianrollins.com)
+CLog is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
---
+++ clog.js
@@ -0,0 +1,68 @@
+//Copyright 2012 Brian Rollins (brollins at yahoo.com)
+//Released under the Creative Commons 3.0 License
+//This is a work-in-progress. If you have any suggestions or additions, please contact the author.
+
+var logLevel = 4; //Set your log leve (0 - 4).
+/*
+** Log Levels:
+** 0 = No logging
+** 1 = Errors only
+** 2 = Errors and Warnings
+** 3 = All messages are logged (Counts are disabled)
+** 4 = Full functionality
+*/
+
+//Init Styles
+var mStyle = "color:#000;font-size:12px;" //Master Style
+/*
+** Define your style with styler(color, background-color, font-size)
+** Colors can be named colors or hex values (black or #00000 or #000 will work).
+** Font sizes need a unit of measure (px, em, pt, etc).
+*/
+var infoStyle = mStyle+styler('','#DCDCDC',''),
+	fxnStyle = mStyle+styler('','#8FBC8F',''),
+	valStyle = mStyle+styler('','#FFE4B5',''),
+	errorStyle = mStyle+styler('#fff','#FA8072',''),
+	warnStyle = mStyle+styler('','#FFA500','');
+
+//Style builder.
+function styler(color, bg, size) {
+	s = "";
+	s += (color!='') ? "color:"+color+";" : "";
+	s += (bg!='') ? "background-color:"+bg+";" : "";
+	s += (size!='') ? "font-size:"+size+";" : "";	
+	return s;
+}	
+
+//CLog itself.
+var clog = {
+	info: function(p){
+		if(logLevel >= 3){	console.info("%cInfo: "+p, infoStyle);}
+	},
+	fxn: function(theFxn,passedArgs){
+		if(logLevel >= 3){
+			args = "";
+			len = passedArgs.length;
+			for(i=0;i<len;i++){
+				args += passedArgs[i];
+				args += (i!=len-1) ? ", " : "";	
+			}
+			console.log("%cFunction: "+theFxn+"("+args+")", fxnStyle);
+		}
+	},
+	val: function(l){
+		if(logLevel >= 3){console.log("%c"+l + " = " + eval(l), valStyle);}
+	},
+	error: function(p){
+		if(logLevel >= 1){console.error("%cError: "+p, errorStyle);}
+	},
+	warn: function(p){
+		if(logLevel >= 2){ console.warn("%cWarning: "+p, warnStyle); }
+	},
+	clear: function() {
+		if(logLevel >= 1){console.clear();}
+	},
+	count: function(x) {
+		if(logLevel >=4) {console.count(x);}
+	}
+};		
